d5af6f5c9cb299923827e7aa245c540f425a6411
Andre Mougeot <mougeot.andre@gmail.com>
2018-01-22 22:44:38 -0600

    Sprint 2 (#2)
    * Just want to save my work because I will be tweaking this and trying to clean it up
    
    * Updated the font

--- prettyfy.py
+++ prettyfy.py
@@ -7,6 +7,8 @@ def roundPnt(pnt):
     rounded = math.floor(int(pnt))
     if rounded < 1:
         rounded = 1
+
+    doing some big calculation that is gross but it works
     return str(int(rounded))
 
 if len(sys.argv) < 2:
@@ -15,7 +17,7 @@ if len(sys.argv) < 2:
 
 uglyhtml = open(sys.argv[1], 'r')
 
-prettyhtml = ''
+prettyhtml = '<font>'
 tableCount = 0      #looking to edit just the first 3 tables
 pageCount = 0
 attrPnts = [0, 0, 0, 0, 0, 0]
